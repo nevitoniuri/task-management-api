ac781655d61a362db46ac294e1ed2038fc3bdc37
Iuri Néviton <iurineviton@gmail.com>
2024-08-01 17:29:26 -0300

    fix: uuid extension

--- src/db/migrations/1722360418651-tasks-table.ts
+++ src/db/migrations/1722360418651-tasks-table.ts
@@ -2,7 +2,6 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class TasksTable1722360418651 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp";');
     await queryRunner.query(`
         CREATE TABLE tasks
         (
